45bb5293c63f35f06405dfe4058802b4af1b24fd
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-21 21:06:36 -0400

    feat(Home): Next SSG concept implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import Header from "../components/Header";
+import { GetStaticProps } from 'next';
+import api from '../services/api';
 
-export default function Home() {
+type Episode = {
+	id: string;
+	title: string;
+	members: string;
+}
+
+type HomeProps = {
+	episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
 	return (
 		<h1>Index</h1>
 	);
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	const { data } = await api.get('episodes', {
+		params: {
+			_limit: 12,
+			_sort: 'published_at',
+			_order: 'desc',
+		}
+	});
+
+	return {
+		props: {
+			episodes: data,
+		},
+		revalidate: 60 * 60 * 8,
+	};
 }
